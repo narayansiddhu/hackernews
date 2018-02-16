b8c20703fbd04ed4205e3ad13ca33f827190ce4b
narayan siddhu <narayansiddhu@gmail.com>
2018-02-16 12:11:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,6 +75,14 @@ app.use(expressValidator({
 }));
 
 // Passport Config
+//Passport's sole purpose is to authenticate requests,
+//which it does through an extensible set of plugins known as strategies.
+//Passport does not mount routes or assume any particular database schema, 
+//which maximizes flexibility and allows application-level decisions to be made by the developer. 
+//The API is simple: you provide Passport a request to authenticate, and 
+//Passport provides hooks for controlling what occurs when authentication succeeds or fails.
+
+
 require('./config/passport')(passport);
 // Passport Middleware
 app.use(passport.initialize());
